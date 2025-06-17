334931c85fb05c48d7a51b8f7e4c081093ba7943
Motar24 <tallboyxx24@gmail.com>
2025-06-17 17:54:11 +0900

    Fix github directry

--- backend/app.py
+++ backend/app.py
@@ -4,14 +4,16 @@ import jwt
 app = Flask(__name__)
 SECRET_KEY = "your_secret_key"
 
+
 @app.route('/api/login', methods=['POST'])
 def login():
-    data = request.json
+    data = request.jsoncd
     username = data.get('username')
     password = data.get('password')
     # ここでDBと照合
     if username == "test" and password == "password":
-        token = jwt.encode({'username': username}, SECRET_KEY, algorithm='HS256')
+        token = jwt.encode({'username': username},
+                           SECRET_KEY, algorithm='HS256')
         return jsonify({'token': token})
     else:
         return jsonify({'error': 'Invalid credentials'}), 401
